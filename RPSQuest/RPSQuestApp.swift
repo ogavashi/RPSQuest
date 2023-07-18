159f79801f805185a980633183722bf843b5dd6e
oleg.gavashi@yahoo.com <oleg.gavashi@yahoo.com>
2023-07-18 12:59:11 +0300

    Initial Commit

---
+++ RPSQuest/RPSQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSQuestApp.swift
+//  RPSQuest
+//
+//  Created by Oleg Gavashi on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RPSQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
